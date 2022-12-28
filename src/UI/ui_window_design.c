f698fd8b274c405ab622febdcfb5373de9dc9da0
Emil D <global52600894@gmail.com>
2022-12-28 23:25:36 +0100

    Updated structures

--- src/UI/ui_window_design.c
+++ src/UI/ui_window_design.c
@@ -9,7 +9,6 @@
 #include "ui_window_design.h"
 #include "ui_window_settings.h"
 
-//Internal prototypes
 void initial_window();
 void main_window();
 void login_window();
@@ -19,58 +18,65 @@ void view_cart_window();
 void search_window();
 void settings_window();
 
-//Struct initialisations
-t_init_window initWindow;
-t_program_status programStatus;
-t_new_window windowSettings;
-t_login_user loginUser;
-t_init_file initFile;
-t_session session;
-t_user_profile registerUserProfile;
-t_user_profile loginUserProfile;
+typedef struct
+{
+    t_init_window initWindow;
+    t_program_status programStatus;
+    t_new_window windowSettings;
+    t_login_user loginUser;
+    t_init_file initFile;
+    t_session session;
+    t_user_profile registerUserProfile;
+    t_user_profile loginUserProfile;
+    t_function_table functionTable[8];
+} t_shared_data;
 
-t_function_table functionTable[] = {
-        {0, "initial_window",  initial_window},
-        {1, "main_window",     main_window},
-        {2, "login_window",    login_window},
-        {3, "register_window", register_window},
-        {4, "view_profile_window", view_profile_window},
-        {5, "view_cart_window", view_cart_window},
-        {6, "search_window", search_window},
-        {7, "settings_window", settings_window}
+static t_shared_data sharedData =
+{
+    .functionTable =
+        {
+            {0, "initial_window",  initial_window},
+            {1, "main_window",     main_window},
+            {2, "login_window",    login_window},
+            {3, "register_window", register_window},
+            {4, "view_profile_window", view_profile_window},
+            {5, "view_cart_window", view_cart_window},
+            {6, "search_window", search_window},
+            {7, "settings_window", settings_window}
+        }
 };
 
 void initialise_ui(float WIDTH, float HEIGHT)
 {
-    if (!initWindow.initialised)
+    if (!sharedData.initWindow.initialised)
     {
         igStyleColorsCustom(NULL);
         //igStyleColorsDark(NULL);
 
-        initFile.file_name = "Userprofiles.txt";
-        initFile.file = NULL;
+        sharedData.initFile.file_name = "Userprofiles.txt";
+        sharedData.initFile.file = NULL;
 
-        windowSettings.WIDTH = WIDTH;
-        windowSettings.HEIGHT = HEIGHT;
+        sharedData.windowSettings.WIDTH = WIDTH;
+        sharedData.windowSettings.HEIGHT = HEIGHT;
 
-        windowSettings.windowflags = 0;
-        windowSettings.windowflags |= ImGuiWindowFlags_NoTitleBar;
-        windowSettings.windowflags |= ImGuiWindowFlags_NoResize;
-        windowSettings.windowflags |= ImGuiWindowFlags_NoMove;
-        windowSettings.windowflags |= ImGuiWindowFlags_NoCollapse;
-        windowSettings.windowflags |= ImGuiWindowFlags_NoScrollbar;
-        windowSettings.windowflags |= ImGuiWindowFlags_NoScrollWithMouse;
-        windowSettings.windowflags |= ImGuiWindowFlags_NoSavedSettings;
-        windowSettings.windowflags |= ImGuiWindowFlags_NoInputs;
+        sharedData.windowSettings.windowflags = 0;
+        sharedData.windowSettings.windowflags |= ImGuiWindowFlags_NoTitleBar;
+        sharedData.windowSettings.windowflags |= ImGuiWindowFlags_NoResize;
+        sharedData.windowSettings.windowflags |= ImGuiWindowFlags_NoMove;
+        sharedData.windowSettings.windowflags |= ImGuiWindowFlags_NoCollapse;
+        sharedData.windowSettings.windowflags |= ImGuiWindowFlags_NoScrollbar;
+        sharedData.windowSettings.windowflags |= ImGuiWindowFlags_NoScrollWithMouse;
+        sharedData.windowSettings.windowflags |= ImGuiWindowFlags_NoSavedSettings;
+        sharedData.windowSettings.windowflags |= ImGuiWindowFlags_NoInputs;
 
-        initialise_initial_window_settings(&windowSettings);
+        initialise_initial_window_settings(&sharedData.windowSettings);
 
-        update_window_settings(&programStatus, &windowSettings, 1);
+        update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 1);
 
-        initWindow.initialised = !initWindow.initialised;
+        sharedData.initWindow.initialised = !sharedData.initWindow.initialised;
     }
 
-    run_new_window(windowSettings, current_active_window(functionTable, programStatus.current_active_window_id));
+    run_new_window(sharedData.windowSettings, current_active_window(sharedData.functionTable, sharedData.programStatus.current_active_window_id));
 }
 
 void initial_window()
@@ -81,13 +87,13 @@ void initial_window()
     igSetCursorPos(Vec2(25, 75));
     if (igButton("Login", Vec2(350, 50)))
     {
-        update_window_settings(&programStatus, &windowSettings, 2);
+        update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 2);
     }
 
     igSetCursorPos(Vec2(25, 130));
     if (igButton("Register", Vec2(350, 50)))
     {
-        update_window_settings(&programStatus, &windowSettings, 3);
+        update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 3);
     }
 }
 
@@ -96,40 +102,40 @@ void main_window()
     igSetCursorPos(Vec2(igGetWindowWidth() * 0.5f - 175, igGetWindowHeight() * 0.5f - 160));
     igBeginGroup();
 
-    time(&session.live_time);
+    time(&sharedData.session.live_time);
 
-    igText("  Welcome %s | Session: %s", loginUserProfile.name, ctime(&session.live_time));
+    igText("  Welcome %s | Session: %s", sharedData.loginUserProfile.name, ctime(&sharedData.session.live_time));
     igNewLine();
 
     if (igButton("View profile", Vec2(350, 50)))
     {
-        update_window_settings(&programStatus, &windowSettings, 4);
+        update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 4);
     }
 
     if (igButton("View cart", Vec2(350, 50)))
     {
-        update_window_settings(&programStatus, &windowSettings, 5);
+        update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 5);
     }
 
     if (igButton("Search items", Vec2(350, 50)))
     {
-        update_window_settings(&programStatus, &windowSettings, 6);
+        update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 6);
     }
 
     if (igButton("Settings", Vec2(350, 50)))
     {
-        update_window_settings(&programStatus, &windowSettings, 7);
+        update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 7);
     }
 
     if (igButton("Logout", Vec2(350, 50)))
     {
-        time(&session.logout_time);
-        printf("Session ended at: %s\n", ctime(&session.logout_time));
+        time(&sharedData.session.logout_time);
+        printf("Session ended at: %s\n", ctime(&sharedData.session.logout_time));
 
-        session.session_time = session.logout_time - session.login_time;
-        session_time_calculator((signed int)session.session_time);
+        sharedData.session.session_time = sharedData.session.logout_time - sharedData.session.login_time;
+        session_time_calculator((signed int)sharedData.session.session_time);
 
-        update_window_settings(&programStatus, &windowSettings, 2);
+        update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 2);
     }
 
     igEndGroup();
@@ -141,35 +147,35 @@ void login_window()
     igBeginGroup();
 
     igText("Username");
-    igInputText("##login_username", loginUser.temp_username, MAX, ImGuiInputTextFlags_CharsNoBlank, NULL, NULL);
+    igInputText("##login_username", sharedData.loginUser.temp_username, MAX, ImGuiInputTextFlags_CharsNoBlank, NULL, NULL);
 
     igText("Password");
-    igInputText("##login_password", loginUser.temp_password, MAX,
+    igInputText("##login_password", sharedData.loginUser.temp_password, MAX,
                 ImGuiInputTextFlags_Password | ImGuiInputTextFlags_CharsNoBlank, NULL, NULL);
 
     igEndGroup();
 
     igSetCursorPos(Vec2(25, 230));
-    igText("Status: %s", programStatus.output_status != NULL ? programStatus.output_status : "...");
+    igText("Status: %s", sharedData.programStatus.output_status != NULL ? sharedData.programStatus.output_status : "...");
 
     igSetCursorPos(Vec2(25, 175));
     if (igButton("Login", Vec2(350, 50)))
     {
-        if (login_operation(initFile.file, initFile.file_name, loginUser, &loginUserProfile))
+        if (login_operation(sharedData.initFile.file, sharedData.initFile.file_name, sharedData.loginUser, &sharedData.loginUserProfile))
         {
-            time(&session.login_time);
-            update_window_settings(&programStatus, &windowSettings, 1);
+            time(&sharedData.session.login_time);
+            update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 1);
         }
         else
         {
-            update_output_status(&programStatus, "Failed to login!");
+            update_output_status(&sharedData.programStatus, "Failed to login!");
         }
     }
 
     igSetCursorPos(Vec2(5, 5));
     if (igButton("<", Vec2(25, 25)))
     {
-        update_window_settings(&programStatus, &windowSettings, 0);
+        update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 0);
     }
 }
 
@@ -181,16 +187,16 @@ void register_window()
     igBeginGroup();
 
     igText("Name");
-    igInputText("##name", registerUserProfile.name, MAX, ImGuiInputTextFlags_CharsNoBlank, NULL, NULL);
+    igInputText("##name", sharedData.registerUserProfile.name, MAX, ImGuiInputTextFlags_CharsNoBlank, NULL, NULL);
 
     igText("Address");
-    igInputText("##address", registerUserProfile.address, MAX, ImGuiInputTextFlags_CharsNoBlank, NULL, NULL);
+    igInputText("##address", sharedData.registerUserProfile.address, MAX, ImGuiInputTextFlags_CharsNoBlank, NULL, NULL);
 
     igText("Username");
-    igInputText("##register_username", registerUserProfile.username, MAX, ImGuiInputTextFlags_CharsNoBlank, NULL, NULL);
+    igInputText("##register_username", sharedData.registerUserProfile.username, MAX, ImGuiInputTextFlags_CharsNoBlank, NULL, NULL);
 
     igText("Password");
-    igInputText("##register_password", registerUserProfile.password, MAX, ImGuiInputTextFlags_CharsNoBlank, NULL, NULL);
+    igInputText("##register_password", sharedData.registerUserProfile.password, MAX, ImGuiInputTextFlags_CharsNoBlank, NULL, NULL);
 
     igEndGroup();
     igPopItemWidth();
@@ -202,44 +208,44 @@ void register_window()
     igBeginGroup();
 
     igText("Age");
-    igInputScalar("##age", ImGuiDataType_U8, &registerUserProfile.age, NULL, NULL, NULL,
+    igInputScalar("##age", ImGuiDataType_U8, &sharedData.registerUserProfile.age, NULL, NULL, NULL,
                   ImGuiInputTextFlags_CharsDecimal);
 
     igText("Longitude");
-    igInputScalar("##longitude", ImGuiDataType_Double, &registerUserProfile.longitude, NULL, NULL, NULL,
+    igInputScalar("##longitude", ImGuiDataType_Double, &sharedData.registerUserProfile.longitude, NULL, NULL, NULL,
                   ImGuiInputTextFlags_CharsDecimal);
 
     igText("Latitude");
-    igInputScalar("##latitude", ImGuiDataType_Double, &registerUserProfile.latitude, NULL, NULL, NULL,
+    igInputScalar("##latitude", ImGuiDataType_Double, &sharedData.registerUserProfile.latitude, NULL, NULL, NULL,
                   ImGuiInputTextFlags_CharsDecimal);
 
     igText("Max traveling distance");
-    igInputScalar("##max_traveling_distance", ImGuiDataType_Double, &registerUserProfile.max_distance, NULL, NULL, NULL,
+    igInputScalar("##max_traveling_distance", ImGuiDataType_Double, &sharedData.registerUserProfile.max_distance, NULL, NULL, NULL,
                   ImGuiInputTextFlags_CharsDecimal);
 
     igEndGroup();
     igPopItemWidth();
 
     igSetCursorPos(Vec2(25, 300));
-    igText("Status: %s", programStatus.output_status != NULL ? programStatus.output_status : "...");
+    igText("Status: %s", sharedData.programStatus.output_status != NULL ? sharedData.programStatus.output_status : "...");
 
     igSetCursorPos(Vec2(25, 245));
     if (igButton("Register", Vec2(350, 50)))
     {
-        if (registration_operation(initFile.file, initFile.file_name, registerUserProfile))
+        if (registration_operation(sharedData.initFile.file, sharedData.initFile.file_name, sharedData.registerUserProfile))
         {
-            update_output_status(&programStatus, "Successfully registered!");
+            update_output_status(&sharedData.programStatus, "Successfully registered!");
         }
         else
         {
-            update_output_status(&programStatus, "Failed to register!");
+            update_output_status(&sharedData.programStatus, "Failed to register!");
         }
     }
 
     igSetCursorPos(Vec2(5, 5));
     if (igButton("<", Vec2(25, 25)))
     {
-        update_window_settings(&programStatus, &windowSettings, 0);
+        update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 0);
     }
 }
 
@@ -249,7 +255,7 @@ void view_profile_window()
     igSetCursorPos(Vec2(5, 5));
     if (igButton("<", Vec2(25, 25)))
     {
-        update_window_settings(&programStatus, &windowSettings, 1);
+        update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 1);
     }
 }
 
@@ -258,7 +264,7 @@ void view_cart_window()
     igSetCursorPos(Vec2(5, 5));
     if (igButton("<", Vec2(25, 25)))
     {
-        update_window_settings(&programStatus, &windowSettings, 1);
+        update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 1);
     }
 }
 
@@ -267,7 +273,7 @@ void search_window()
     igSetCursorPos(Vec2(5, 5));
     if (igButton("<", Vec2(25, 25)))
     {
-        update_window_settings(&programStatus, &windowSettings, 1);
+        update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 1);
     }
 }
 
@@ -276,6 +282,6 @@ void settings_window()
     igSetCursorPos(Vec2(5, 5));
     if (igButton("<", Vec2(25, 25)))
     {
-        update_window_settings(&programStatus, &windowSettings, 1);
+        update_window_settings(&sharedData.programStatus, &sharedData.windowSettings, 1);
     }
 }
--- src/UI/ui_window_design.h
+++ src/UI/ui_window_design.h
@@ -1,8 +1,8 @@
 #ifndef P1_EXTENSION_USER_INTERFACE_H
 #define P1_EXTENSION_USER_INTERFACE_H
 
-#endif //P1_EXTENSION_USER_INTERFACE_H
-
 //External prototypes
 void initialise_ui(float WIDTH, float HEIGHT);
+
+#endif //P1_EXTENSION_USER_INTERFACE_H
 
